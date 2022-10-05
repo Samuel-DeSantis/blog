01c2f0c6b4a7a5df79f52348b014796a169c84d4
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2022-10-04 22:07:32 -0600

    updating views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Post.create(
+  title: "Getting Started with Rails", 
+  summary: "A brief overview on how to get started with using Ruby on Rails.", 
+  content: "Content for the article goes here."
+)
+
+Post.create(
+  title: "Getting Started with Django", 
+  summary: "A brief overview on how to get started with using Django.", 
+  content: "Content for the article goes here."
+)
+
+Post.create(
+  title: "Getting Started with Ruby", 
+  summary: "A brief overview on how to get started with using Ruby.", 
+  content: "Content for the article goes here."
+)
+
+Post.create(
+  title: "Getting Started with Python", 
+  summary: "A brief overview on how to get started with using Python.", 
+  content: "Content for the article goes here."
+)
+
+Post.create(
+  title: "", 
+  summary: "", 
+  content: ""
+)
