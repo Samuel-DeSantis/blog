5d093a9afb35b351c15adff41521b6a2ddc9cdd3
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2022-10-05 23:35:19 -0600

    adding post formatting

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+
+  def format
+
+  end
+
+  def line_break
+    paragraphs = self.content.split("[br]")
+    paragraphs.collect! { |paragraph| "<p>" + paragraph + "</p></br>" }
+    paragraphs.join()
+  end
+
+  def code
+  end
+
+  def bold
+  end
+
+  def underline
+  end
+
 end
