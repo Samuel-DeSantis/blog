403558b67269e504fdc67b2abe0d080672eeab9b
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2022-10-08 20:56:58 -0600

    updating formatting

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,25 +1,123 @@
 class Post < ApplicationRecord
 
   def format
+    effects = {
+      "[code]": {
+        open: "<code>",
+        close: "</code>"
+      },
+      "[bold]": {
+        open: "<b>",
+        close: "</b>"
+      },
+      "[underline]": {
+        open: "<u>",
+        close: "</u>"
+      },
+      "[bq]": {
+        open: "<blockquote class='blockquote'>",
+        close: "</blockquote>"
+      },
+      "[author]": {
+        open: "<figcaption class='blockquote-footer'>",
+        close: "</figcaption>"
+      },
+      "[br]": "<br>"
+    }
 
+    effects.each do |key, value|
+      status = true
+      while self.content.include?(key.to_s)
+        if key.to_s == "[br]"
+          self.content.gsub!(key.to_s, value) 
+        else
+          if status
+            self.content.sub!(key.to_s, value[:open])
+            status = false
+          else
+            self.content.sub!(key.to_s, value[:close])
+            status = true          
+          end
+        end   
+      end
+    end
+
+    self.content
   end
 
-  def line_break
-    paragraphs = self.content.split("[br]")
-    paragraphs.collect! { |paragraph| "<p>" + paragraph + "</p></br>" }
-    paragraphs.join()
+
+          # effects = [
+    #   method(:line_break), 
+    #   method(:code), 
+    #   method(:bold), 
+    #   method(:underline)
+    # ]
+    # effects[0].call(content)
+    # effects.each { |fx| fx.call(content) }
+
+    # content = code(content)
+    # content = line_break(content)
+    # content = bold(content)
+    # content = underline(content)
+  def line_break (content)
+    parse = content.split("[br]")
+    parse.collect! { |text| wrap_p_tag(text) + "</br>" }
+    parse.join()
   end
 
-  def code
-    paragraphs = self.content.split("[code]")
-    paragraphs.collect! { |paragraph| "<code>" + paragraph + "</code>" }
-    paragraphs.join()
+  def code (content)
+    between(content, "[code]", method(:wrap_code_tag))
   end
 
-  def bold
+  def bold (content)
+    between(content, "[b]", method(:wrap_bold_tag))
   end
 
-  def underline
+  def underline (content)
+    between(content, "[u]", method(:wrap_underline_tag))
+  end
+
+  def between (content, tag, fx)
+    parse = content.split(tag)
+    if parse.count.odd?
+      parse.collect!.each_with_index { |text, index|
+        if index.odd?
+          fx.call(text)
+        else
+          text
+        end
+      }.join()
+    else
+      "ERROR:[CODE]_TAG_MISSING_CLOSURE=>" + content
+    end
+  end
+  
+  def html_wrap (text, tag)
+    add_carrots(tag) + text + add_carrots(tag)
+  end
+
+  def wrap_p_tag (text)
+    "<p>" + text + "</p>"
+  end
+
+  def wrap_code_tag (text)
+    "<code>" + text + "</code>"
+  end
+
+  def wrap_bold_tag (text)
+    "<b>" + text + "</b>"
+  end
+
+  def wrap_underline_tag (text)
+    "<u>" + text + "</u>"
+  end
+
+  def add_brackets (tag)
+    "[" + tag + "]"
+  end
+
+  def add_carrots (tag)
+    "<" + tag + ">"
   end
 
 end
